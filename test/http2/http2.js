d1c056f1b07ad89f1a7eab0fefbcb01d33654268
Nilesh Mali <nileshmali86@gmail.com>
2018-05-10 19:16:56 +0530

    Disable failing test

--- test/http2/http2.js
+++ test/http2/http2.js
@@ -8,7 +8,8 @@ if (semver.gt(process.versions.node, '9.0.0')) {
   require('./full-post-http2')
   require('./http-http2')
   require('./http2-target-crash')
-  require('./http2-target-multi-crash')
+  // TODO: Enable after https://github.com/nodejs/node/issues/20630 fix
+  // require('./http2-target-multi-crash')
   require('./http2-invalid-target')
 } else {
   tap.pass('Skip because Node version < 9.0.0')
