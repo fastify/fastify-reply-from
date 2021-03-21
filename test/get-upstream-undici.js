78070c01c2a909c82d244194625a3bebc701b475
yohayg@gmail.com <yohayg@gmail.com>
2021-03-21 21:55:28 +0200

    added getUpstream test for undici and http

---
+++ test/get-upstream-undici.js
@@ -0,0 +1,46 @@
+'use strict'
+
+const t = require('tap')
+const Fastify = require('fastify')
+const From = require('..')
+const http = require('http')
+const get = require('simple-get').concat
+
+const instance = Fastify()
+instance.register(From, {
+  base: 'http://localhost',
+  disableCache: true
+})
+
+t.plan(7)
+t.tearDown(instance.close.bind(instance))
+
+const target = http.createServer((req, res) => {
+  t.pass('request proxied')
+  t.equal(req.method, 'GET')
+  res.end(req.headers.host)
+})
+
+instance.get('/test', (request, reply) => {
+  reply.from('/test', {
+    getUpstream: (req, base) => {
+      t.pass('getUpstream called')
+      return `${base}:${target.address().port}`
+    }
+  })
+})
+
+t.tearDown(target.close.bind(target))
+
+instance.listen(0, (err) => {
+  t.error(err)
+
+  target.listen(0, (err) => {
+    t.error(err)
+
+    get(`http://localhost:${instance.server.address().port}/test`, (err, res) => {
+      t.error(err)
+      t.equal(res.statusCode, 200)
+    })
+  })
+})
