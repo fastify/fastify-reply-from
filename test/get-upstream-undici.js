bfce097d2e6fbc2c221726ff46e234864e18c823
yohayg@gmail.com <yohayg@gmail.com>
2021-03-22 10:32:59 +0200

    increase test coverage

--- test/get-upstream-undici.js
+++ test/get-upstream-undici.js
@@ -7,19 +7,12 @@ const http = require('http')
 const get = require('simple-get').concat
 
 const instance = Fastify()
-const instanceWithoutBase = Fastify()
 instance.register(From, {
-  base: 'http://localhost',
   disableCache: true
 })
 
-instanceWithoutBase.register(From, {
-  disableCache: true
-})
-
-t.plan(13)
+t.plan(7)
 t.tearDown(instance.close.bind(instance))
-t.tearDown(instanceWithoutBase.close.bind(instanceWithoutBase))
 
 const target = http.createServer((req, res) => {
   t.pass('request proxied')
@@ -29,15 +22,6 @@ const target = http.createServer((req, res) => {
 
 instance.get('/test', (request, reply) => {
   reply.from('/test', {
-    getUpstream: (req, base) => {
-      t.pass('getUpstream called')
-      return `${base}:${target.address().port}`
-    }
-  })
-})
-
-instanceWithoutBase.get('/test2', (request, reply) => {
-  reply.from('/test2', {
     getUpstream: () => {
       t.pass('getUpstream called')
       return `http://localhost:${target.address().port}`
@@ -49,20 +33,13 @@ t.tearDown(target.close.bind(target))
 
 instance.listen(0, (err) => {
   t.error(err)
-  instanceWithoutBase.listen(0, (err) => {
+
+  target.listen(0, (err) => {
     t.error(err)
-    target.listen(0, (err) => {
+
+    get(`http://localhost:${instance.server.address().port}/test`, (err, res) => {
       t.error(err)
-
-      get(`http://localhost:${instance.server.address().port}/test`, (err, res) => {
-        t.error(err)
-        t.equal(res.statusCode, 200)
-      })
-
-      get(`http://localhost:${instanceWithoutBase.server.address().port}/test2`, (err, res) => {
-        t.error(err)
-        t.equal(res.statusCode, 200)
-      })
+      t.equal(res.statusCode, 200)
     })
   })
 })
