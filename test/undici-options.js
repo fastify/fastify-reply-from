1fba6518a977b124bf9a17b4d802b7f687eff93b
Jinwoo Oh <arkist@gmail.com>
2021-06-28 12:28:05 -0700

    fix: undici options should apply on handleUndici

--- test/undici-options.js
+++ test/undici-options.js
@@ -3,44 +3,71 @@
 const t = require('tap')
 const Fastify = require('fastify')
 const proxyquire = require('proxyquire')
-
-t.plan(1)
-
-class Agent {
-  constructor (opts) {
-    t.strictSame(opts, {
-      connections: 42,
-      pipelining: 24,
-      keepAliveTimeout: 4242,
-      tls: {
-        rejectUnauthorized: false
-      }
-    })
-  }
-}
-
-// original setup in the undici module
-// needed to test a bug
-function undici () {}
-undici.Agent = Agent
-
-const buildRequest = proxyquire('../lib/request.js', {
-  undici
-})
-
-const From = proxyquire('..', {
-  './lib/request.js': buildRequest
-})
+const http = require('http')
+const get = require('simple-get').concat
+const undici = require('undici')
+const { getUndiciOptions } = require('../lib/request')
 
 const instance = Fastify()
 
-instance.register(From, {
-  base: 'http://path/to/somewhere',
-  undici: {
-    connections: 42,
-    pipelining: 24,
-    keepAliveTimeout: 4242
-  }
+t.plan(7)
+t.teardown(instance.close.bind(instance))
+
+const target = http.createServer((req, res) => {
+  res.statusCode = 200
+  res.end('hello world')
 })
 
-instance.ready()
+instance.get('/', (request, reply) => {
+  reply.from()
+})
+
+t.teardown(target.close.bind(target))
+
+target.listen(0, err => {
+  t.error(err)
+
+  const From = proxyquire('..', {
+    './lib/request.js': proxyquire('../lib/request.js', {
+      undici: undiciProxy
+    })
+  })
+
+  instance.register(From, {
+    base: `http://localhost:${target.address().port}`,
+    undici: buildUndiciOptions()
+  })
+
+  instance.listen(0, err => {
+    t.error(err)
+
+    get(`http://localhost:${instance.server.address().port}`, (err, res, data) => {
+      t.error(err)
+      t.equal(res.statusCode, 200)
+      t.equal(data.toString(), 'hello world')
+    })
+  })
+})
+
+function undiciProxy () {}
+undiciProxy.Agent = class Agent extends undici.Agent {
+  constructor (opts) {
+    super(opts)
+    t.strictSame(opts, buildUndiciOptions())
+  }
+}
+undiciProxy.Pool = class Pool extends undici.Pool {
+  constructor (url, options) {
+    super(url, options)
+    t.hasStrict(options, buildUndiciOptions())
+  }
+}
+
+function buildUndiciOptions () {
+  return getUndiciOptions({
+    connections: 42,
+    pipelining: 24,
+    keepAliveTimeout: 4242,
+    strictContentLength: false
+  })
+}
