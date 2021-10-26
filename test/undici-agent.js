6a31087a14558289dedc11650a3dcbc3dde42c5d
KaKa <climba03003@gmail.com>
2021-10-26 14:45:52 +0800

    fix: spwan pool on request when use agent (#207)
    * fix: spwan pool on request when use agent
    
    * test: ensure pool is not spawn twice for same host

--- test/undici-agent.js
+++ test/undici-agent.js
@@ -8,9 +8,9 @@ const get = require('simple-get').concat
 const undici = require('undici')
 const { getUndiciOptions } = require('../lib/request')
 
-const instance = Fastify()
+t.plan(10)
 
-t.plan(6)
+const instance = Fastify()
 t.teardown(instance.close.bind(instance))
 
 const target = http.createServer((req, res) => {
@@ -26,10 +26,20 @@ t.teardown(target.close.bind(target))
 
 target.listen(0, err => {
   t.error(err)
+  let poolCreation = 0
 
   const From = proxyquire('..', {
     './lib/request.js': proxyquire('../lib/request.js', {
-      undici: undiciProxy
+      undici: proxyquire('undici', {
+        './lib/agent': proxyquire('undici/lib/agent.js', {
+          './pool': class Pool extends undici.Pool {
+            constructor (url, options) {
+              super(url, options)
+              poolCreation++
+            }
+          }
+        })
+      })
     })
   })
 
@@ -45,23 +55,17 @@ target.listen(0, err => {
       t.error(err)
       t.equal(res.statusCode, 200)
       t.equal(data.toString(), 'hello world')
+      t.equal(poolCreation, 1)
+
+      get(`http://localhost:${instance.server.address().port}`, (err, res, data) => {
+        t.error(err)
+        t.equal(res.statusCode, 200)
+        t.equal(data.toString(), 'hello world')
+        t.equal(poolCreation, 1)
+      })
     })
   })
 })
-
-function undiciProxy () {}
-undiciProxy.Agent = class Agent extends undici.Agent {
-  constructor (opts) {
-    super(opts)
-    t.strictSame(opts, buildUndiciOptions())
-  }
-}
-undiciProxy.Pool = class Pool extends undici.Pool {
-  constructor (url, options) {
-    super(url, options)
-    t.hasStrict(options, buildUndiciOptions())
-  }
-}
 
 function buildUndiciOptions () {
   return getUndiciOptions({
