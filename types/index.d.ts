1a67cc74964931aff45eb6509c1f1d36beadb49d
Uzlopak <aras.abbasi@googlemail.com>
2022-11-22 13:03:47 +0100

    nodenext compatibility (#280)
    * nodenext compatibility
    
    * fix merge error

--- types/index.d.ts
+++ types/index.d.ts
@@ -30,68 +30,76 @@ import {
   SecureClientSessionOptions,
 } from "http2";
 import { Pool } from 'undici'
-type QueryStringFunction = (search: string | undefined, reqUrl: string) => string;
-export interface FastifyReplyFromHooks {
-  queryString?: { [key: string]: unknown } | QueryStringFunction;
-  contentType?: string;
-  onResponse?: (
-    request: FastifyRequest<RequestGenericInterface, RawServerBase>,
-    reply: FastifyReply<RawServerBase>,
-    res: RawReplyDefaultExpression<RawServerBase>
-  ) => void;
-  onError?: (
-      reply: FastifyReply<RawServerBase>,
-      error: { error: Error }
-  ) => void;
-  body?: unknown;
-  rewriteHeaders?: (
-    headers: Http2IncomingHttpHeaders | IncomingHttpHeaders,
-    req?: Http2ServerRequest | IncomingMessage
-  ) => Http2IncomingHttpHeaders | IncomingHttpHeaders;
-  rewriteRequestHeaders?: (
-    req: Http2ServerRequest | IncomingMessage,
-    headers: Http2IncomingHttpHeaders | IncomingHttpHeaders
-  ) => Http2IncomingHttpHeaders | IncomingHttpHeaders;
-  getUpstream?: (
-      req: Http2ServerRequest | IncomingMessage,
-      base: string
-  ) => string;
-}
 
 declare module "fastify" {
   interface FastifyReply {
     from(
       source?: string,
-      opts?: FastifyReplyFromHooks
+      opts?: fastifyReplyFrom.FastifyReplyFromHooks
     ): this;
   }
 }
 
-interface Http2Options {
-  sessionTimeout?: number;
-  requestTimeout?: number;
-  sessionOptions?: ClientSessionOptions | SecureClientSessionOptions;
-  requestOptions?: ClientSessionRequestOptions;
+type FastifyReplyFrom = FastifyPluginCallback<fastifyReplyFrom.FastifyReplyFromOptions>
+
+declare namespace fastifyReplyFrom {
+  type QueryStringFunction = (search: string | undefined, reqUrl: string) => string;
+  export interface FastifyReplyFromHooks {
+    queryString?: { [key: string]: unknown } | QueryStringFunction;
+    contentType?: string;
+    onResponse?: (
+      request: FastifyRequest<RequestGenericInterface, RawServerBase>,
+      reply: FastifyReply<RawServerBase>,
+      res: RawReplyDefaultExpression<RawServerBase>
+    ) => void;
+    onError?: (
+      reply: FastifyReply<RawServerBase>,
+      error: { error: Error }
+    ) => void;
+    body?: unknown;
+    rewriteHeaders?: (
+      headers: Http2IncomingHttpHeaders | IncomingHttpHeaders,
+      req?: Http2ServerRequest | IncomingMessage
+    ) => Http2IncomingHttpHeaders | IncomingHttpHeaders;
+    rewriteRequestHeaders?: (
+      req: Http2ServerRequest | IncomingMessage,
+      headers: Http2IncomingHttpHeaders | IncomingHttpHeaders
+    ) => Http2IncomingHttpHeaders | IncomingHttpHeaders;
+    getUpstream?: (
+      req: Http2ServerRequest | IncomingMessage,
+      base: string
+    ) => string;
+  }
+
+  interface Http2Options {
+    sessionTimeout?: number;
+    requestTimeout?: number;
+    sessionOptions?: ClientSessionOptions | SecureClientSessionOptions;
+    requestOptions?: ClientSessionRequestOptions;
+  }
+
+  interface HttpOptions {
+    agentOptions?: AgentOptions | SecureAgentOptions;
+    requestOptions?: RequestOptions | SecureRequestOptions;
+    agents?: { 'http:': Agent, 'https:': SecureAgent }
+  }
+
+  export interface FastifyReplyFromOptions {
+    base?: string;
+    cacheURLs?: number;
+    disableCache?: boolean;
+    http?: HttpOptions;
+    http2?: Http2Options | boolean;
+    undici?: Pool.Options;
+    contentTypesToEncode?: string[];
+    retryMethods?: (HTTPMethods | 'TRACE')[];
+    maxRetriesOn503?: number;
+    disableRequestLogging?: boolean;
+  }
+
+  export const fastifyReplyFrom: FastifyReplyFrom
+  export { fastifyReplyFrom as default }
 }
 
-interface HttpOptions {
-  agentOptions?: AgentOptions | SecureAgentOptions;
-  requestOptions?: RequestOptions | SecureRequestOptions;
-  agents?: { 'http:': Agent, 'https:': SecureAgent }
-}
-
-export interface FastifyReplyFromOptions {
-  base?: string;
-  cacheURLs?: number;
-  disableCache?: boolean;
-  http?: HttpOptions;
-  http2?: Http2Options | boolean;
-  undici?: Pool.Options;
-  contentTypesToEncode?: string[];
-  retryMethods?: (HTTPMethods | 'TRACE')[];
-  maxRetriesOn503?: number;
-  disableRequestLogging?: boolean;
-}
-
-declare const fastifyReplyFrom: FastifyPluginCallback<FastifyReplyFromOptions>;
-export default fastifyReplyFrom;
+declare function fastifyReplyFrom(...params: Parameters<FastifyReplyFrom>): ReturnType<FastifyReplyFrom>
+export = fastifyReplyFrom
