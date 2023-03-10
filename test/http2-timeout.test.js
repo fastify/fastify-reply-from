e15bf197d216813012d2fc8e1649cad6479a52c8
Dean Haleem <deanhaleem.10@gmail.com>
2023-03-10 15:03:17 -0500

    add http2 fix back in

--- test/http2-timeout.test.js
+++ test/http2-timeout.test.js
@@ -92,3 +92,34 @@ test('http2 session timeout', async (t) => {
 
   t.fail()
 })
+
+test('http2 sse removes request and session timeout test', async (t) => {
+  const target = Fastify({ http2: true, sessionTimeout: 0 })
+
+  target.get('/', (request, reply) => {
+    t.pass('request arrives')
+
+    reply.status(200).header('content-type', 'text/event-stream').send('hello world')
+  })
+
+  await target.listen({ port: 0 })
+
+  const instance = Fastify()
+
+  instance.register(From, {
+    base: `http://localhost:${target.server.address().port}`,
+    http2: { sessionTimeout: 100 }
+  })
+
+  instance.get('/', (request, reply) => {
+    reply.from(`http://localhost:${target.server.address().port}/`)
+  })
+
+  await instance.listen({ port: 0 })
+
+  t.teardown(instance.close.bind(instance))
+  t.teardown(target.close.bind(target))
+
+  const { statusCode } = await got.get(`http://localhost:${instance.server.address().port}/`, { retry: 0 })
+  t.equal(statusCode, 200)
+})
