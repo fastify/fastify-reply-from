dbdab53db7c56f488d7bf21c792f2f5f3eb99de0
Ivan Tymoshenko <bugagashenkj@gmail.com>
2023-05-21 18:33:21 +0200

    use static undici.request instead of pool.request

--- test/undici-custom-dispatcher.test.js
+++ test/undici-custom-dispatcher.test.js
@@ -1,57 +1,63 @@
 'use strict'
 
-const t = require('tap')
-const get = require('simple-get').concat
+const { test } = require('tap')
 const undici = require('undici')
 const Fastify = require('fastify')
 const FastifyUndiciDispatcher = require('fastify-undici-dispatcher')
 const From = require('..')
 
-const target = Fastify()
-target.get('/', (req, reply) => {
-  t.pass('request proxied')
-  t.equal(req.headers.connection, 'keep-alive')
-
-  reply.headers({
-    'Content-Type': 'text/plain',
-    'x-my-header': 'hello!'
+test('use a custom instance of \'undici\'', async t => {
+  const target = Fastify({
+    keepAliveTimeout: 1
   })
 
-  reply.statusCode = 205
-  reply.send('hello world')
-})
+  target.get('/', (req, reply) => {
+    t.pass('request proxied')
 
-t.test('use a custom instance of \'undici\'', async t => {
-  t.teardown(target.close.bind(target))
-
-  await new Promise((resolve, reject) => target.listen({ port: 0 }, err => err ? reject(err) : resolve()))
-
-  t.test('custom undici dispatcher', t => {
-    const instance = Fastify()
-    t.teardown(instance.close.bind(instance))
-
-    const fastifyDispatcher = new FastifyUndiciDispatcher(new undici.Agent())
-    fastifyDispatcher.route('myserver.local', target)
-
-    instance.register(From, {
-      undici: fastifyDispatcher
+    reply.headers({
+      'Content-Type': 'text/plain',
+      'x-my-header': 'hello!'
     })
 
-    instance.get('/', (request, reply) => {
-      reply.from('http://myserver.local')
-    })
-
-    instance.listen({ port: 0 }, (err) => {
-      t.error(err)
-
-      get(`http://localhost:${instance.server.address().port}`, (err, res, data) => {
-        t.error(err)
-        t.equal(res.headers['content-type'], 'text/plain')
-        t.equal(res.headers['x-my-header'], 'hello!')
-        t.equal(res.statusCode, 205)
-        t.equal(data.toString(), 'hello world')
-        t.end()
-      })
-    })
+    reply.statusCode = 205
+    reply.send('hello world')
   })
+
+  await target.listen({ port: 0 })
+  t.teardown(async () => {
+    await target.close()
+  })
+
+  const instance = Fastify({
+    keepAliveTimeout: 1
+  })
+
+  const fastifyDispatcher = new FastifyUndiciDispatcher(new undici.Agent())
+  t.teardown(() => {
+    fastifyDispatcher.close()
+  })
+
+  fastifyDispatcher.route('myserver.local', target)
+
+  instance.register(From, {
+    undici: fastifyDispatcher
+  })
+
+  instance.get('/', (request, reply) => {
+    reply.from('http://myserver.local')
+  })
+
+  await instance.listen({ port: 0 })
+  t.teardown(async () => {
+    await instance.close()
+  })
+
+  const res = await undici.request(`http://localhost:${instance.server.address().port}`)
+
+  t.equal(res.headers['content-type'], 'text/plain')
+  t.equal(res.headers['x-my-header'], 'hello!')
+  t.equal(res.statusCode, 205)
+
+  const data = await res.body.text()
+  t.equal(data, 'hello world')
 })
