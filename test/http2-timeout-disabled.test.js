65b22778fcb8e1c41a1f4a0a25f66d05216157ad
Gunter Spranz <gunter@gspranz.de>
2024-05-07 17:16:59 +0200

    allow explicitly disabling request and session timeouts with 0

---
+++ test/http2-timeout-disabled.test.js
@@ -0,0 +1,48 @@
+'use strict'
+
+const { test } = require('tap')
+const Fastify = require('fastify')
+const From = require('..')
+const got = require('got')
+
+test('http2 request timeout disabled', async (t) => {
+  const target = Fastify({ http2: true, sessionTimeout: 0 })
+  t.teardown(target.close.bind(target))
+
+  target.get('/', () => {
+    t.pass('request arrives')
+  })
+
+  await target.listen({ port: 0 })
+
+  const instance = Fastify()
+  t.teardown(instance.close.bind(instance))
+
+  instance.register(From, {
+    base: `http://localhost:${target.server.address().port}`,
+    http2: { requestTimeout: 0, sessionTimeout: 16000 }
+  })
+
+  instance.get('/', (request, reply) => {
+    reply.from(`http://localhost:${target.server.address().port}/`)
+  })
+
+  await instance.listen({ port: 0 })
+
+  try {
+    await Promise.all([
+      got.get(`http://localhost:${instance.server.address().port}/`, {
+        retry: 0
+      }),
+      new Promise((resolve, reject) => setTimeout(reject, 11000, 'passed'))
+    ])
+  } catch (err) {
+    // if we wait 11000ms without a timeout error, we assume disabling the timeout worked
+    // 10000 ms is the default timeout
+    t.equal(err, 'passed')
+
+    return
+  }
+
+  t.fail()
+})
