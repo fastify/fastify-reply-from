c52152aabd96ec44bee3904ee8cc2eddb80bda95
Aras Abbasi <aras.abbasi@googlemail.com>
2024-12-29 22:54:07 +0100

    chore: remove semver (#395)

--- test/head-with-body.test.js
+++ test/head-with-body.test.js
@@ -5,12 +5,6 @@ const Fastify = require('fastify')
 const From = require('..')
 const http = require('node:http')
 const get = require('simple-get').concat
-const semver = require('semver')
-
-if (semver.gte(process.version, '20.2.0')) {
-  t.comment('skip this test on node >= 20.2.0 as it is current broken')
-  process.exit(0)
-}
 
 const instance = Fastify()
 
