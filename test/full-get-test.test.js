393afc7a23e7bc539f6489f897a56d2bf310a8d1
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2025-03-04 22:04:40 +0100

    feat: removed simple-get

--- test/full-get-test.test.js
+++ test/full-get-test.test.js
@@ -4,7 +4,6 @@ const t = require('tap')
 const Fastify = require('fastify')
 const From = require('..')
 const http = require('node:http')
-const get = require('simple-get').concat
 
 const instance = Fastify()
 instance.register(From)
@@ -31,7 +30,7 @@ t.teardown(target.close.bind(target))
 instance.listen({ port: 0 }, (err) => {
   t.error(err)
 
-  target.listen({ port: 0 }, (err) => {
+  target.listen({ port: 0 }, async (err) => {
     t.error(err)
 
     const result = await fetch(`https://localhost:${instance.server.address().port}`)
