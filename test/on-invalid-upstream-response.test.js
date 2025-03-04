5a30c3b5ba45effb6f5ab12b8d259eec7ae65ac4
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2025-03-04 22:11:34 +0100

    feat: removed simple-get

--- test/on-invalid-upstream-response.test.js
+++ test/on-invalid-upstream-response.test.js
@@ -4,12 +4,11 @@ const t = require('tap')
 const Fastify = require('fastify')
 const From = require('..')
 const http = require('node:http')
-const get = require('simple-get').concat
 
 const instance = Fastify()
 instance.register(From)
 
-t.plan(8)
+t.plan(7)
 t.teardown(instance.close.bind(instance))
 
 const target = http.createServer((req, res) => {
@@ -32,21 +31,16 @@ t.teardown(target.close.bind(target))
 instance.listen({ port: 0 }, (err) => {
   t.error(err)
 
-  target.listen({ port: 0 }, (err) => {
+  target.listen({ port: 0 }, async (err) => {
     t.error(err)
 
-    get(
-      `http://localhost:${instance.server.address().port}`,
-      (err, res, data) => {
-        t.error(err)
-        t.equal(res.statusCode, 502)
-        t.same(JSON.parse(data), {
-          statusCode: 502,
-          code: 'FST_REPLY_FROM_BAD_GATEWAY',
-          error: 'Bad Gateway',
-          message: 'Bad Gateway'
-        })
-      }
-    )
+    const result = await fetch(`http://localhost:${instance.server.address().port}`)
+    t.equal(result.status, 502)
+    t.equal(await result.json(), {
+      statusCode: 502,
+      code: 'FST_REPLY_FROM_BAD_GATEWAY',
+      error: 'Bad Gateway',
+      message: 'Bad Gateway'
+    })
   })
 })
