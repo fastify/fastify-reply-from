94060004eb0cae6dfa12250120c8e330f54dbddd
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2025-03-05 08:44:05 +0100

    feat: migrate full rewrite

--- test/get-upstream-type.test.js
+++ test/get-upstream-type.test.js
@@ -4,14 +4,13 @@ const t = require('tap')
 const Fastify = require('fastify')
 const From = require('..')
 const http = require('node:http')
-const get = require('simple-get').concat
 
 const instance = Fastify()
 instance.register(From, {
   disableCache: true
 })
 
-t.plan(8)
+t.plan(7)
 t.teardown(instance.close.bind(instance))
 
 const target = http.createServer((req, res) => {
@@ -35,12 +34,11 @@ t.teardown(target.close.bind(target))
 instance.listen({ port: 0 }, (err) => {
   t.error(err)
 
-  target.listen({ port: 0 }, (err) => {
+  target.listen({ port: 0 }, async (err) => {
     t.error(err)
 
-    get(`http://localhost:${instance.server.address().port}`, (err, res) => {
-      t.error(err)
-      t.equal(res.statusCode, 200)
-    })
+    const result = await fetch(`http://localhost:${instance.server.address().port}`)
+
+    t.equal(result.status, 200)
   })
 })
