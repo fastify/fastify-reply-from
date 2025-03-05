a66951fcc16c5d878eccf4da2be8788168b24ddd
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2025-03-05 08:44:34 +0100

    feat: migrate full rewrite

--- test/get-upstream-undici.test.js
+++ test/get-upstream-undici.test.js
@@ -4,7 +4,6 @@ const t = require('tap')
 const Fastify = require('fastify')
 const From = require('..')
 const http = require('node:http')
-const get = require('simple-get').concat
 
 const instance = Fastify()
 instance.register(From, {
@@ -34,12 +33,11 @@ t.teardown(target.close.bind(target))
 instance.listen({ port: 0 }, (err) => {
   t.error(err)
 
-  target.listen({ port: 0 }, (err) => {
+  target.listen({ port: 0 }, async (err) => {
     t.error(err)
 
-    get(`http://localhost:${instance.server.address().port}/test`, (err, res) => {
-      t.error(err)
-      t.equal(res.statusCode, 200)
-    })
+    const result = await fetch(`http://localhost:${instance.server.address().port}/test`)
+
+    t.equal(result.status, 200)
   })
 })
