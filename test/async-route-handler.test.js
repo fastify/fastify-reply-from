5134a5bc7e9c5085daded9b186e7373150832a13
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2025-03-08 19:24:44 +0100

    fix: async route handler

--- test/async-route-handler.test.js
+++ test/async-route-handler.test.js
@@ -4,47 +4,46 @@ const t = require('tap')
 const Fastify = require('fastify')
 const From = require('..')
 const http = require('node:http')
-const get = require('simple-get').concat
+const { request } = require('undici')
 
 const instance = Fastify()
 
-t.plan(11)
-t.teardown(instance.close.bind(instance))
+t.test('async route handler', async (t) => {
+  t.plan(8)
+  t.teardown(instance.close.bind(instance))
 
-const target = http.createServer((req, res) => {
-  t.pass('request proxied')
-  t.equal(req.method, 'GET')
-  t.equal(req.url, '/')
-  res.statusCode = 205
-  res.setHeader('Content-Type', 'text/plain')
-  res.setHeader('x-my-header', 'hello!')
-  res.end('hello world')
-})
+  const target = http.createServer((req, res) => {
+    t.pass('request proxied')
+    t.equal(req.method, 'GET')
+    t.equal(req.url, '/')
+    res.statusCode = 205
+    res.setHeader('Content-Type', 'text/plain')
+    res.setHeader('x-my-header', 'hello!')
+    res.end('hello world')
+  })
 
-instance.get('/', async (_request, reply) => {
-  const p = reply.from()
-  t.equal(p, reply)
-  return p
-})
+  instance.get('/', async (_request, reply) => {
+    const p = reply.from()
+    t.equal(p, reply)
+    return p
+  })
 
-t.teardown(target.close.bind(target))
+  t.teardown(target.close.bind(target))
 
-target.listen({ port: 0 }, (err) => {
-  t.error(err)
+  await new Promise(resolve => target.listen({ port: 0 }, resolve))
 
   instance.register(From, {
     base: `http://localhost:${target.address().port}`
   })
 
-  instance.listen({ port: 0 }, (err) => {
-    t.error(err)
+  await new Promise(resolve => instance.listen({ port: 0 }, resolve))
 
-    get(`http://localhost:${instance.server.address().port}`, (err, res, data) => {
-      t.error(err)
-      t.equal(res.headers['content-type'], 'text/plain')
-      t.equal(res.headers['x-my-header'], 'hello!')
-      t.equal(res.statusCode, 205)
-      t.equal(data.toString(), 'hello world')
-    })
-  })
+  const result = await request(`http://localhost:${instance.server.address().port}`)
+
+  t.equal(result.headers['content-type'], 'text/plain')
+  t.equal(result.headers['x-my-header'], 'hello!')
+  t.equal(result.statusCode, 205)
+  t.equal(await result.body.text(), 'hello world')
+
+  await instance.close()
 })
