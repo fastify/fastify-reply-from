975a7341382eb7d73b36aca0f31e21ccbe325cf9
leonied7 <kdnn@mail.ru>
2025-03-26 22:50:19 +0700

    feat: add specific timeout for request

--- test/http2-timeout.test.js
+++ test/http2-timeout.test.js
@@ -4,6 +4,7 @@ const { test } = require('tap')
 const Fastify = require('fastify')
 const From = require('..')
 const got = require('got')
+const FakeTimers = require('@sinonjs/fake-timers')
 
 test('http2 request timeout', async (t) => {
   const target = Fastify({ http2: true, sessionTimeout: 0 })
@@ -43,6 +44,63 @@ test('http2 request timeout', async (t) => {
       message: 'Gateway Timeout'
     })
 
+    return
+  }
+
+  t.fail()
+})
+
+test('http2 request with specific timeout', async (t) => {
+  const clock = FakeTimers.createClock()
+  const target = Fastify({ http2: true })
+  t.teardown(target.close.bind(target))
+
+  target.get('/', (_request, reply) => {
+    t.pass('request arrives')
+
+    setTimeout(() => {
+      reply.status(200).send('hello world')
+    }, 200)
+
+    clock.tick(200)
+  })
+
+  await target.listen({ port: 0 })
+
+  const instance = Fastify()
+  t.teardown(instance.close.bind(instance))
+
+  instance.register(From, {
+    base: `http://localhost:${target.server.address().port}`,
+    http2: { requestTimeout: 100, sessionTimeout: 6000 }
+  })
+
+  instance.get('/success', (_request, reply) => {
+    reply.from(`http://localhost:${target.server.address().port}/`, {
+      timeout: 300
+    })
+  })
+  instance.get('/fail', (_request, reply) => {
+    reply.from(`http://localhost:${target.server.address().port}/`, {
+      timeout: 50
+    })
+  })
+
+  await instance.listen({ port: 0 })
+  const { statusCode } = await got.get(`http://localhost:${instance.server.address().port}/success`, { retry: 0 })
+  t.equal(statusCode, 200)
+
+  try {
+    await got.get(`http://localhost:${instance.server.address().port}/fail`, { retry: 0 })
+  } catch (err) {
+    t.equal(err.response.statusCode, 504)
+    t.match(err.response.headers['content-type'], /application\/json/)
+    t.same(JSON.parse(err.response.body), {
+      statusCode: 504,
+      code: 'FST_REPLY_FROM_GATEWAY_TIMEOUT',
+      error: 'Gateway Timeout',
+      message: 'Gateway Timeout'
+    })
     return
   }
 
--- test/undici-timeout.test.js
+++ test/undici-timeout.test.js
@@ -1,24 +1,24 @@
 'use strict'
 
-const t = require('tap')
+const { test } = require('tap')
 const Fastify = require('fastify')
 const From = require('..')
 const got = require('got')
 const FakeTimers = require('@sinonjs/fake-timers')
 
-const clock = FakeTimers.createClock()
-
-t.test('undici request timeout', async (t) => {
+test('undici request timeout', async (t) => {
+  const clock = FakeTimers.createClock()
   const target = Fastify()
   t.teardown(target.close.bind(target))
 
   target.get('/', (_request, reply) => {
     t.pass('request arrives')
 
-    clock.setTimeout(() => {
+    setTimeout(() => {
       reply.status(200).send('hello world')
-      t.end()
     }, 1000)
+
+    clock.tick(1000)
   })
 
   await target.listen({ port: 0 })
@@ -50,7 +50,66 @@ t.test('undici request timeout', async (t) => {
       error: 'Gateway Timeout',
       message: 'Gateway Timeout'
     })
+    return
+  }
+
+  t.fail()
+})
+
+test('undici request with specific timeout', async (t) => {
+  const clock = FakeTimers.createClock()
+  const target = Fastify()
+  t.teardown(target.close.bind(target))
+
+  target.get('/', (_request, reply) => {
+    t.pass('request arrives')
+
+    setTimeout(() => {
+      reply.status(200).send('hello world')
+    }, 1000)
+
     clock.tick(1000)
+  })
+
+  await target.listen({ port: 0 })
+
+  const instance = Fastify()
+  t.teardown(instance.close.bind(instance))
+
+  instance.register(From, {
+    base: `http://localhost:${target.server.address().port}`,
+    undici: {
+      headersTimeout: 100,
+    }
+  })
+
+  instance.get('/success', (_request, reply) => {
+    reply.from('/', {
+      timeout: 1000
+    })
+  })
+  instance.get('/fail', (_request, reply) => {
+    reply.from('/', {
+      timeout: 50
+    })
+  })
+
+  await instance.listen({ port: 0 })
+
+  const { statusCode } = await got.get(`http://localhost:${instance.server.address().port}/success`, { retry: 0 })
+  t.equal(statusCode, 200)
+
+  try {
+    await got.get(`http://localhost:${instance.server.address().port}/fail`, { retry: 0 })
+  } catch (err) {
+    t.equal(err.response.statusCode, 504)
+    t.match(err.response.headers['content-type'], /application\/json/)
+    t.same(JSON.parse(err.response.body), {
+      statusCode: 504,
+      code: 'FST_REPLY_FROM_GATEWAY_TIMEOUT',
+      error: 'Gateway Timeout',
+      message: 'Gateway Timeout'
+    })
     return
   }
 
